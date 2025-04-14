600609b51dfb1895e0efc64c974d2e315a43ed8a
SamuelRab <samuelrabelodasilva@gmail.com>
2025-04-14 15:52:54 -0300

    Samuel é bonito

---
+++ atv2/index.js
@@ -0,0 +1,15 @@
+let soma = 0; // Variável que armazena a soma.
+
+for (let i = 1; i <= 10; i++) {
+    soma = soma + parseInt(prompt("Insira um número:")) // Variável que armazena o número digitado pleo usuário.
+
+    /*
+        Estrutura do loop for:
+        - Inicialização: let i = 1 -> começamos o contador i em 1.
+        - Condição i<= num -> 0 loop continua enquanto i for menor ou igual a num
+        (variável que armazena o número do usuário)
+        - Atualização : i++ -> A cada repetição, i aumenta em 1
+        */
+}
+alert(soma)
+
